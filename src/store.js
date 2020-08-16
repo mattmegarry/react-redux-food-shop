b99d209a79414b55d5f1403ccdab9c6ff3069a8f
Matt Megarry <mattmegarry@gmail.com>
2020-08-17 00:01:57 +0100

    Add redux devtools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import foodsReducer from "./reducers/foodsReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(foodsReducer);
+const store = createStore(foodsReducer, composeWithDevTools());
 
 export default store;
